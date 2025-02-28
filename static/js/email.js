9b63b30dbfcde42fdc3e3a034982268a41f1ad51
Mark Evola <markevola24@gmail.com>
2025-02-28 14:36:01 -0600

    basic modal

--- static/js/email.js
+++ static/js/email.js
@@ -1,6 +1,33 @@
-function initalizeApp() {}
+function attachListeners() {
+  const composeForm = document.getElementById("compose-form");
+  const modal = document.getElementById("modal-bg");
+  const newEmailForm = document.getElementById("new-email-form");
+  const closeBtn = document.getElementById("close-modal");
 
-function cleanupApp() {}
+  if (!composeForm || !modal || !newEmailForm || !closeBtn) {
+    console.error("Compose button not found");
+    return;
+  }
 
-window.initalizeApp = initalizeApp;
-window.cleanupApp = cleanupApp;
+  composeForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    modal.style.display = "block";
+  });
+
+  newEmailForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+    modal.style.display = "none";
+    
+    
+  });
+
+  closeBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    modal.style.display = "none";
+  });
+}
+
+window.initializeApp = function () {
+  attachListeners();
+};
+window.cleanupApp = function () {};
